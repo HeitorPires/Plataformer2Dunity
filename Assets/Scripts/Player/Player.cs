6ad99a75b0d48d63ada3daf16c79d5ef35c2f1cf
HeitorPires <heitorpprocopio@hotmail.com>
2024-03-31 02:46:18 -0400

    Add player jump and land animation and start the camera implementation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _myRigidiBody;
 
+    [Header("Speed setup")]
     public float speed = 10;
     public float speedRun = 20;
-    private float _currentSpeed;
-
-    public Vector2 friction = new Vector2(-.3f, 0);
     public float jumpForce = 15;
+
+    [Header("Animation setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .7f;
+    public float jumpAnimationDuration = .2f;
+    public Ease ease = Ease.Linear;
+    public float landScaleX = 1.5f;
+    public float landScaleY = .7f;
+    public float landAnimationDuration = .2f;
+
+    private Vector2 friction = new Vector2(-.3f, 0);
+    private float _currentSpeed;
+    private bool _isGrounded = false;
 
     private void Awake()
     {
@@ -50,6 +61,45 @@ public class Player : MonoBehaviour
     private void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            _myRigidiBody.velocity = Vector2.up * jumpForce; 
+        {
+            _myRigidiBody.velocity = Vector2.up * jumpForce;
+            _myRigidiBody.transform.localScale = Vector2.one;
+            DOTween.Kill(_myRigidiBody.transform);
+            HandleScaleJump();
+            _isGrounded = false;
+        }
     }
+
+    private void HandleScaleJump()
+    {
+        _myRigidiBody.transform.DOScaleY(jumpScaleY, jumpAnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        _myRigidiBody.transform.DOScaleX(jumpScaleX, jumpAnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+    private void HandleScaleLand()
+    {
+        _myRigidiBody.transform.localScale = Vector2.one;
+        DOTween.Kill(_myRigidiBody.transform);
+        _myRigidiBody.transform.DOScaleX(landScaleX, landAnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        _myRigidiBody.transform.DOScaleY(landScaleY, landAnimationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            if(!_isGrounded)
+            {
+                HandleScaleLand();
+                _isGrounded = true;
+            }
+        }
+
+    }
+
+    
+
+
+
 }
