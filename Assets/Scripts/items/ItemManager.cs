4e1c18cc65e011abd9d683835a9f653596b0633f
HeitorPires <heitorpprocopio@hotmail.com>
2024-04-04 17:11:06 -0400

    Add update for coin counter GUI

--- Assets/Scripts/items/ItemManager.cs
+++ Assets/Scripts/items/ItemManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Core.Singleton;
+using TMPro;
 
 public class ItemManager : Singleton<ItemManager>
 {
-
+    public TextMeshProUGUI textMeshPro;
     public int coins;
 
     private void Start()
@@ -16,10 +17,17 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        UpdateCoinsGUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateCoinsGUI();
+    }
+
+    public void UpdateCoinsGUI()
+    {
+        textMeshPro.text = $"X{coins}";
     }
 }
