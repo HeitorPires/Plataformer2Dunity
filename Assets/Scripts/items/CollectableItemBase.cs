78ad6de22fa6b186f48650a0f3c196de0ce6ff97
HeitorPires <heitorpprocopio@hotmail.com>
2024-04-10 17:08:24 -0400

    Add coins particles

--- Assets/Scripts/items/CollectableItemBase.cs
+++ Assets/Scripts/items/CollectableItemBase.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class CollectableItemBase : MonoBehaviour
 {
+    private bool _canCollect = true;
+    public string playerTag = "Player";
+    public ParticleSystem particleSystem;
+    public float timeToDestroy = 3f;
+    public GameObject graphicItem;
 
-    public string compareTag = "Player";
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(compareTag))
+        if (_canCollect && collision.gameObject.CompareTag(playerTag))
+        {
+            _canCollect = false;
             Collect();
+        }
     }
 
     protected virtual void Collect() 
     {
         OnCollect();
-        Destroy(gameObject);
     }
 
-    protected virtual void OnCollect() { }
+    private void HideObject()
+    {
+        if (graphicItem != null)
+            graphicItem.gameObject.SetActive(false);
+    }
+
+    private void PlayParticleSystem()
+    {
+        
+        if(particleSystem != null)
+            particleSystem.Play();
+
+    }
+
+    protected virtual void OnCollect() 
+    { 
+        HideObject();
+        PlayParticleSystem();
+        Destroy(gameObject, timeToDestroy);
+    }
 }
